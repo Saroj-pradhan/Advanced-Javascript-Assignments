af1eb44c9ae11bbc1b4172ce7c30e4a23d821434
saroj <sarojpradhan7655@gmail.com>
2026-02-13 13:06:03 +0530

    solve<Promise easy> solved Promises  promiseRace assignment

--- promises/easy/promiseRace.js
+++ promises/easy/promiseRace.js
@@ -4,7 +4,26 @@
 // The function should accept an iterable of values that may include Promises or plain values. 
 // It must settle as soon as the first input settles, resolving or rejecting accordingly. 
 // Using Promise.resolve ensures non-promise values are handled correctly.
-function promiseRace(promises) {}
+function promiseRace(promises) {
+    let len = promises.length;
+   
+   
+    return new Promise((resolve,reject)=>{
+         if(len == 0) {};
+    for(let i=0;i< len ; i++){
+        async function Run(){
+         try {
+            const result = await Promise.resolve(promises[i]);
+            resolve(result);
+        } catch (error) {
+                reject(error);
+        }
+        }
+        Run();
+       
+    }
+    })
+}
 
 module.exports = promiseRace;
 
